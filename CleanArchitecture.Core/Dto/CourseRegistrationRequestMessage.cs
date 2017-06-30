b6e862d4806a5225361b9e24d4b3f8c2b3d535aa
Mark Macneil <mark.macneil@t4g.com>
2017-06-29 22:21:22 -0300

    post in progress

--- CleanArchitecture.Core/Dto/CourseRegistrationRequestMessage.cs
+++ CleanArchitecture.Core/Dto/CourseRegistrationRequestMessage.cs
@@ -1,11 +1,17 @@
 ﻿ 
 
+using System.Collections.Generic;
 using CleanArchitecture.Core.Contracts;
 
 namespace CleanArchitecture.Core.Dto
 {
     public class CourseRegistrationRequestMessage : IRequest<CourseRegistrationResponseMessage>
     {
+        public List<string> SelectedCourseCodes { get; private set; }
 
+        public CourseRegistrationRequestMessage(List<string> selectedCourseCodes)
+        {
+            SelectedCourseCodes = selectedCourseCodes;
+        }
     }
 }
